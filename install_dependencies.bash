a0bc535cbd2219c07844b3ae8a2e071779efdcc6
Clayder Gonzalez <claydergc@gmail.com>
2024-05-24 15:23:23 +1000

    Update install_dependencies.bash

--- install_dependencies.bash
+++ install_dependencies.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt-get install libgoogle-glog-dev libgflags-dev libjsoncpp-dev libeigen3-dev nvidia-cuda-toolkit
+#sudo apt-get install libgoogle-glog-dev libgflags-dev libjsoncpp-dev libeigen3-dev nvidia-cuda-toolkit
 
 wget https://download.pytorch.org/libtorch/cu102/libtorch-cxx11-abi-shared-with-deps-1.7.0.zip
 
